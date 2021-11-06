6bdfdde2e356eac545631ada96a8529379a4b67a
Saket Sinha <saketsinha23@gmail.com>
2021-11-06 23:55:12 +0530

    fix: lint error

--- src/parserTypes.ts
+++ src/parserTypes.ts
@@ -1,16 +1,16 @@
 export type frameObj = {
-    currentIndex: number,
-    data: ImageData | undefined,
-    totalFrames: number,
-}
+  currentIndex: number;
+  data: ImageData | undefined;
+  totalFrames: number;
+};
 export type frameChange = (obj: frameObj) => void;
 export type onCompletionLoopObj = (iterationCount: number, frameLength: number) => void;
 
 export type constructorObjType = {
-    gifSource: string, 
-    on_frame_change: frameChange, 
-    canvasElement : HTMLCanvasElement,
-    renderGif: boolean,
-    handleOnCompleteLoop: onCompletionLoopObj,
-    delay: number,
-}
+  gifSource: string;
+  on_frame_change: frameChange;
+  canvasElement: HTMLCanvasElement;
+  renderGif: boolean;
+  handleOnCompleteLoop: onCompletionLoopObj;
+  delay: number;
+};
